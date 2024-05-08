4c2f65a8e819d1312b50413a7a2de41e1a41bcea
Lucas7Alves <lucassalvess0909@gmail.com>
2024-05-08 19:21:48 -0300

    Add router do Colaborador

--- API_AGENDAMENTOS/routers/ColaboradorRoute.js
+++ API_AGENDAMENTOS/routers/ColaboradorRoute.js
@@ -2,14 +2,62 @@ const { Router } = require("express");
 const router = Router();
 const conexao = require("../infraestrutura/conexao");
 
+//solicitar agendamento
+router.post("/solicitar", (req, res) => {
+  const id_fornecedor = req.body.id_fornecedor;
+  const tipo_carga = req.body.tipo_carga;
+  const tipo_descarga = req.body.tipo_descarga;
+  const recorrencia = req.body.recorrencia;
+  const observacao = req.body.observacao;
+  const data_entrega = req.body.data;
+  const hora_entrega  = req.body.horario;
+  const status = 'P';
+
+  const sql = ` INSERT INTO AGENDAMENTO (ID_AGENDAMENTO, ID_FORNECEDOR, TIPO_CARGA, TIPO_DESCARGA, RECORRENCIA, OBSERVACAO, HORA_ENTREGA, DATA_ENTREGA, STATUS) VALUES ('${idAgendamento}', '${id_fornecedor}', '${tipo_carga}', '${tipo_descarga}', '${recorrencia}', '${observacao}', '${hora_entrega}', '${data_entrega}', '${status}')`
+
+  conexao.query(sql, (error, results) => {
+    
+    //Tratando erros
+    if (error) {
+      console.log("Erro ao criar a solicitação!");
+      console.log(error.message);
+      res.status(500).json({ message: "Erro ao criar a solicitação!" });
+    } else if (results.changedRows === 0) {
+      console.log("Agendamento não foi criado");
+      res.status(404).json({ message: "A solicitação não foi criada!" });
+    } else {
+      console.log("Agendamento criado com sucesso");
+      res.status(200).json({ message: "Solicitação criada com sucesso!" });
+    }
+  });
+})
+
+
 //delete agendamento
-router.delete("/agendamentos/:id", (req, res) => {
-  res.destroy({where: {'id': req.params.id}}).then(function() {
-    res.send("Deletado!")
-  }).catch(function(erro) {
-    res.send("Não existe esse ID!")
+router.delete("/agendamentos/:id/:status", (req, res) => {
+  const id_fornecedor = req.params.id;
+  const status = req.params.status;
+  
+  const sql = 
+  `
+  DELETE FROM AGENDAMENTO WHERE ID_AGENDAMENTO = '${id_fornecedor}' AND STATUS = '${status}'
+  `
+
+  conexao.query(sql, (error, results) => {
+    
+    //Tratando erros
+   if (error) {
+      console.log("Erro ao deletar a solicitação!");
+      console.log(error.message);
+      res.status(500).json({ message: "Erro ao deletar a solicitação!" });
+    } else if (results.affectedRows === 0) {
+      console.log("Solicitação não encontrado!");
+      res.status(404).json({ message: "Solicitação não encontrada!" });
+    } else {
+      console.log("Solicitação deletada com sucesso!");
+      res.status(200).json({ message: "Solicitação deletada com sucesso!" });
+    }
   });
 });
-
 
 module.exports = router;
--- API_AGENDAMENTOS/routers/FornecedorRoute.js
+++ API_AGENDAMENTOS/routers/FornecedorRoute.js
@@ -9,31 +9,31 @@ router.post("/agendamento/criar", (req, res) => {
   const cnpj = req.body.cnpj;
   const idAgendamento = req.body.idAgendamento;
   const matricula_colaborador = req.body.matricula_colaborador;
-  const tipo_cacarga = req.body.tipo_cacarga;
+  const tipo_carga = req.body.tipo_cacarga;
   const tipo_descarga = req.body.tipo_descarga;
   const recorrencia = req.body.recorrencia;
   const observacao = req.body.observacao;
   const hora_entrega = req.body.hora_entrega;
   const data_entrega = req.body.data_entrega;
-  const status = req.body.status;
+  const status = 'P';
 
   //Comando para inserir os dados no banco de dados
-  const sql = `INSERT INTO AGENDAMENTO (CNPJ, ID_AGENDAMENTO, MATRICULA_COLABORADOR, TIPO_CACARGA, TIPO_DESCARGA, RECORRENCIA, OBSERVACAO, HORA_ENTREGA, DATA_ENTREGA, STATUS) VALUES ('${cnpj}', '${idAgendamento}', '${matricula_colaborador}', '${tipo_cacarga}', '${tipo_descarga}', '${recorrencia}', '${observacao}', '${hora_entrega}', '${data_entrega}', '${status}')`;
+  const sql = `INSERT INTO AGENDAMENTO (CNPJ, ID_AGENDAMENTO, MATRICULA_COLABORADOR, TIPO_CARGA, TIPO_DESCARGA, RECORRENCIA, OBSERVACAO, HORA_ENTREGA, DATA_ENTREGA, STATUS) VALUES ('${cnpj}', '${idAgendamento}', '${matricula_colaborador}', '${tipo_carga}', '${tipo_descarga}', '${recorrencia}', '${observacao}', '${hora_entrega}', '${data_entrega}', '${status}')`;
 
   //Inserindo dados no banco de dados
   conexao.query(sql, (error, results) => {
     
     //Tratando erros
     if (error) {
-      console.log("Erro ao deletar o agendamento");
+      console.log("Erro ao criar o agendamento!");
       console.log(error.message);
-      res.status(500).json({ message: "Erro ao criar o agendamento" });
+      res.status(500).json({ message: "Erro ao criar o agendamento!" });
     } else if (results.changedRows === 0) {
       console.log("Agendamento não foi criado");
-      res.status(404).json({ message: "Agendamento não foi criado" });
+      res.status(404).json({ message: "Agendamento não foi criado!" });
     } else {
-      console.log("Agendamento criado com sucesso");
-      res.status(200).json({ message: "Agendamento criado com sucesso" });
+      console.log("Agendamento criado com sucesso!");
+      res.status(200).json({ message: "Agendamento criado com sucesso!" });
     }
   });
 });
@@ -50,15 +50,15 @@ router.delete("/agendamento/:id/:status", (req, res) => {
 
     //Tratando erros
     if (error) {
-      console.log("Erro ao deletar o agendamento");
+      console.log("Erro ao deletar o agendamento!");
       console.log(error.message);
-      res.status(500).json({ message: "Erro ao deletar o agendamento" });
+      res.status(500).json({ message: "Erro ao deletar o agendamento!" });
     } else if (results.affectedRows === 0) {
-      console.log("Agendamento não encontrado");
-      res.status(404).json({ message: "Agendamento não encontrado" });
+      console.log("Agendamento não encontrado!");
+      res.status(404).json({ message: "Agendamento não encontrado!" });
     } else {
-      console.log("Agendamento deletado com sucesso");
-      res.status(200).json({ message: "Agendamento deletado com sucesso" });
+      console.log("Agendamento deletado com sucesso!");
+      res.status(200).json({ message: "Agendamento deletado com sucesso!" });
     }
   });
 });
