5c2be8c2787c8c890893627193f95392ea3ae270
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2024-06-06 22:19:40 -0300

    Testes na rota colaborador
    ajustes na tabela SQL + adição da função de leitura de arquivo .json no index principal do projeto API.

--- API_AGENDAMENTOS/index.js
+++ API_AGENDAMENTOS/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const router = require("./routers/index");
-const conexao = require("./infraestrutura/conexao")
-const tabelas = require("./infraestrutura/tabelas")
+const conexao = require("./infraestrutura/conexao");
+const tabelas = require("./infraestrutura/tabelas");
 
 tabelas.init(conexao);
+app.use(express.json());
 
 router(app);
 
--- API_AGENDAMENTOS/routers/ColaboradorRoute.js
+++ API_AGENDAMENTOS/routers/ColaboradorRoute.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const conexao = require("../infraestrutura/conexao");
+const { act } = require("react");
 
 //solicitar agendamento
 router.post("/solicitar", (req, res) => {
@@ -11,40 +12,36 @@ router.post("/solicitar", (req, res) => {
   const tipo_descarga = req.body.tipo_descarga;
   const recorrencia = req.body.recorrencia;
   const observacao = req.body.observacao;
-  const data_entrega = req.body.data;
-  const hora_entrega = req.body.horario;
+  const data_entrega = req.body.data_entrega;
+  const hora_entrega = req.body.hora_entrega;
   const status = "P";
 
-  const sql = ` INSERT INTO AGENDAMENTO (ID_AGENDAMENTO, CNPJ, TIPO_CARGA, TIPO_DESCARGA, RECORRENCIA, OBSERVACAO, HORA_ENTREGA, DATA_ENTREGA, STATUS) VALUES ('${idAgendamento}', '${cnpj}', '${tipo_carga}', '${tipo_descarga}', '${recorrencia}', '${observacao}', '${hora_entrega}', '${data_entrega}', '${status}')`;
+  const sql = ` INSERT INTO AGENDAMENTO (ID_AGENDAMENTO, CNPJ, MATRICULA_COLABORADOR, TIPO_CARGA, TIPO_DESCARGA, RECORRENCIA, OBSERVACAO, HORA_ENTREGA, DATA_ENTREGA, STATUS) VALUES ('${idAgendamento}', '${cnpj}', '${matricula_colaborador}', '${tipo_carga}', '${tipo_descarga}', '${recorrencia}', '${observacao}', '${hora_entrega}', '${data_entrega}', '${status}')`;
 
   conexao.query(sql, (error, results) => {
     //Tratando erros
     if (error) {
-      console.log("Erro ao criar a solicitação!");
-      console.log(error.message);
-      res.status(500).json({ message: "Erro ao criar a solicitação!" });
+      res
+        .status(500)
+        .json({ message: "Erro ao criar a solicitação: " + error });
     } else if (results.changedRows === 0) {
-      console.log("Agendamento não foi criado");
-      res.status(404).json({ message: "A solicitação não foi criada!" });
+      res.status(200).json({ message: "A solicitação foi criada!" });
     } else {
-      console.log("Agendamento criado com sucesso");
       res.status(200).json({ message: "Solicitação criada com sucesso!" });
-      logColaborador("solicitar", matricula_colaborador, cnpj, idAgendamento)
+      logColaborador("solicitar", matricula_colaborador, cnpj, idAgendamento);
     }
   });
 });
 
-
 //delete agendamento
-router.delete("/delete/:id/:status", (req, res) => {
-  const cnpj = req.params.id;
-  const status = req.params.status;
+router.delete("/delete", (req, res) => {
+  const idAgendamento = req.body.id;
+  const status = req.body.status;
 
-  const sql = 
-  `
-  DELETE FROM AGENDAMENTO WHERE ID_AGENDAMENTO = '${cnpj}' AND STATUS = '${status}'
+  const sql = `
+  DELETE FROM AGENDAMENTO WHERE ID_AGENDAMENTO = '${idAgendamento}' AND STATUS = '${status}'
   `;
-  
+
   conexao.query(sql, (error, results) => {
     //Tratando erros
     if (error) {
@@ -64,18 +61,19 @@ router.delete("/delete/:id/:status", (req, res) => {
 //Ler agendamentos pendentes
 router.get("/pendentes", (req, res) => {
   const sql = `
-  SELECT * FROM AGENDAMENTO WHERE STATUS = P
+  SELECT * FROM AGENDAMENTO WHERE STATUS = 'P'
   `;
-  
-  conexao.query(sql, (erro, result) => {
-    if (err) {
+
+  conexao.query(sql, (error, result) => {
+    if (error) {
       console.log("Erro ao consultar tabela!");
       console.log(error.message);
       res.status(500).json({ message: "Erro ao consultar tabela!" });
     } else {
       console.log("Tabela consultada com sucesso!");
-      res.status(200).json({ message: "Tabela consultada com sucesso!" });
-      res.json(result);
+      res
+        .status(200)
+        .json({ message: "Tabela consultada com sucesso!", data: result });
     }
   });
 });
@@ -83,51 +81,52 @@ router.get("/pendentes", (req, res) => {
 //Ler agendamentos finalizados
 router.get("/finalizados", (req, res) => {
   const sql = `
-  SELECT * FROM AGENDAMENTO WHERE STATUS = F
+  SELECT * FROM AGENDAMENTO WHERE STATUS = 'F'
   `;
-  
-  conexao.query(sql, (erro, result) => {
-    if (err) {
+
+  conexao.query(sql, (error, result) => {
+    if (error) {
       console.log("Erro ao consultar tabela!");
       console.log(error.message);
       res.status(500).json({ message: "Erro ao consultar tabela!" });
     } else {
       console.log("Tabela consultada com sucesso!");
-      res.status(200).json({ message: "Tabela consultada com sucesso!" });
-      res.json(result);
+      res
+        .status(200)
+        .json({ message: "Tabela consultada com sucesso!", data: result });
     }
   });
 });
 
 //resposta
 router.put("/resposta", (req, res) => {
-  if (req.body.resposta === "aceitar") {
-    const idAgendamento = req.body.idAgendamento
-    sql = 
-    `
-    UPDATE AGENDAMENTO SET
-    STATUS = A WHERE ID_AGENDAMENTO = '${idAgendamento}'
-    `
-    conexao.query(log, (error, result) => {
+  const resposta = req.body.resposta;
+  const idAgendamento = req.body.idAgendamento;
+  const matricula_colaborador = req.body.matricula_colaborador;
+  const cnpj = req.body.cnpj;
+  if (resposta === "aceitar") {
+    sql = `
+    UPDATE AGENDAMENTO SET STATUS = 'A' WHERE ID_AGENDAMENTO = '${idAgendamento}'
+    `;
+    conexao.query(sql, (error, response) => {
       if (error) {
         console.log("Erro ao aceitar!");
         console.log(error.message);
         res.status(500).json({ message: "Erro ao aceitar!" });
       } else {
         console.log("Aceitado!");
-        res.status(200).json({ message: "Aceite realizado com sucesso!" });
-        res.json(result);
+        res.status(200).json({ message: "Aceite realizado com sucesso!" }); //
+        logColaborador(resposta, matricula_colaborador, cnpj, idAgendamento);
       }
     });
   } else if (req.body.resposta === "recusar") {
-    const idAgendamento = req.body.idAgendamento
-    sql = 
-    `
+    const idAgendamento = req.body.idAgendamento;
+    sql = `
     UPDATE AGENDAMENTO SET
-    STATUS = R WHERE ID_AGENDAMENTO = '${idAgendamento}'
-    `
-    
-    conexao.query(log, (error, result) => {
+    STATUS = 'R' WHERE ID_AGENDAMENTO = '${idAgendamento}'
+    `;
+
+    conexao.query(sql, (error, result) => {
       if (error) {
         console.log("Erro ao recusar!");
         console.log(error.message);
@@ -135,26 +134,27 @@ router.put("/resposta", (req, res) => {
       } else {
         console.log("Recusado!");
         res.status(200).json({ message: "Recusa realizada com sucesso!" });
-        res.json(result);
+        logColaborador(resposta, matricula_colaborador, cnpj, idAgendamento);
       }
     });
-  } else if (req.body.resposta === "finalizado") {
-    const idAgendamento = req.body.idAgendamento
-    sql = 
-    `
+  } else if (req.body.resposta === "finalizar") {
+    const idAgendamento = req.body.idAgendamento;
+    sql = `
     UPDATE AGENDAMENTO SET
-    STATUS = F WHERE ID_AGENDAMENTO = '${idAgendamento}'
-    `
+    STATUS = 'F' WHERE ID_AGENDAMENTO = '${idAgendamento}'
+    `;
 
-    conexao.query(log, (error, result) => {
+    conexao.query(sql, (error, result) => {
       if (error) {
         console.log("Erro ao finalizar!");
         console.log(error.message);
         res.status(500).json({ message: "Erro ao finalizar agendamento!" });
       } else {
         console.log("Finalizado!");
-        res.status(200).json({ message: "Agendamento finalizado com sucesso!" });
-        res.json(result);
+        res
+          .status(200)
+          .json({ message: "Agendamento finalizado com sucesso!" });
+        logColaborador(resposta, matricula_colaborador, cnpj, idAgendamento);
       }
     });
   }
@@ -164,101 +164,35 @@ router.put("/resposta", (req, res) => {
 router.post("/loginColaborador", (req, res) => {
   const matricula = req.body.matricula;
   const senha = req.body.senha;
-  
-  if (matricula && senha) {
+
+  if (matricula && senha) { 
     const sql = `
-    SELECT * FROM FORNECEDOR WHERE MATRICULA = "${matricula}" AND SENHA = "${senha}"; 
+    SELECT * FROM COLABORADOR WHERE MATRICULA_COLABORADOR = '${matricula}' AND SENHA_COLABORADOR = '${senha}'; 
     `;
     conexao.query(sql, (error, result) => {
+
       if (result.length > 0) {
-        for (let i = 0; i < result.length; i++) {
-          if (result[i].senha == this.senha) {
-            req.session.matricula = result[i].matricula;
-            res.status(200).json({ message: "Login efetuado com sucesso!" });
-            res.json(result);
-            res.redirect("/");
-          } else {
-            console.log(error.message);
-            res.status(500).json({ message: "Matrícula ou senha inválido" });
-            res.status("");
-          }
+        if (result[0].SENHA_COLABORADOR === senha) {
+          res.status(200).json({ message: "Login efetuado com sucesso!"});
+        } else {
+          res.status(500).json({ message: "Matrícula ou senha inválido" });
         }
       } else {
         res.status(500).json({ message: "Matrícula ou senha inválido" });
       }
-      res.end();
     });
   } else {
-    res.send("Insira um Id e uma senha válida");
-    res.end();
+    res.status(400).json({ message: "Insira uma matrícula e uma senha válida" });
   }
 });
 
 
 // Inserindo modificações na tabela log
 function logColaborador(action, matricula, cnpj, idAgendamento) {
-  if (action === "solicitar") {
-    log = 
-    `
+    log = `
     INSERT INTO LOG (TIPO, MATRICULA_COLABORADOR, CNPJ, ID_AGENDAMENTO, DATA_CRIACAO)
     VALUES ( ${action}, '${matricula}, ${cnpj}, ${idAgendamento}, CURRENT_TIMESTAMP')
-    `
-    conexao.query(log, (error, result) => {
-      if (error) {
-        console.log("Erro ao solicitar!");
-        console.log(error.message);
-        res.status(500).json({ message: "Erro ao solicitar!" });
-      } else {
-        console.log("Solicitado!");
-        res.status(200).json({ message: "Solicitação realizada com sucesso!" });
-        res.json(result);
-      }
-    });
-
-  } else if (action === "deletar") {
-    log = 
-    `
-    INSERT INTO LOG (TIPO, MATRICULA_COLABORADOR, CNPJ, ID_AGENDAMENTO, DATA_CRIACAO)
-    VALUES ( ${action}, '${matricula}, ${cnpj}, ${idAgendamento}, CURRENT_TIMESTAMP')
-    `
-
-  conexao.query(log, (error, result) => {
-    if (error) {
-      console.log("Erro ao registrar o delete na tabela log!");
-      console.log(error.message);
-      res.status(500).json({ message: "Erro ao registrar o delete na tabela log!" });
-    } else {
-      console.log("Deletado!");
-      res.status(200).json({ message: "Registro realizado com sucesso!" });
-      res.json(result);
-    }
-  });
-  } else if (action === "modificar") {
-    log - 
-    `
-    UPDATE AGENDAMENTO SET
-    CNPJ = '${cnpj}',
-    TIPO_CARGA = '${tipo_carga}',
-    TIPO_DESCARGA = '${tipo_descarga}',
-    RECORRENCIA = '${recorrencia}',
-    OBSERVACAO = '${observacao}',
-    HORA_ENTREGA = '${hora_entrega}',
-    DATA_ENTREGA = '${data_entrega}'  WHERE ID_AGENDAMENTO = ${idAgendamento} 
-    `
-
-    conexao.query(log, (error, result) => {
-      if (error) {
-        console.log("Erro ao registrar na tabela log!");
-        console.log(error.message);
-        res.status(500).json({ message: "Erro ao registrar na tabela log!" });
-      } else {
-        console.log("Modificado!");
-        res.status(200).json({ message: "Registrado com sucesso!" });
-        res.json(result);
-      }
-    });
-  }
+    `;
 }
-
 
 module.exports = router;
